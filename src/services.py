cb354c8e793c02965829b153f3b4cfdd64dfb88d
natalia_yepifanova <epif97@mail.ru>
2024-12-16 11:24:20 +0300

    add func search_by_transfers_to_individuals

--- src/services.py
+++ src/services.py
@@ -1,11 +1,13 @@
-from datetime import datetime
 import json
-import math
-from typing import Any, Dict, Hashable, List, final
-from src.reading_excel_file import reading_excel
 import logging
+import math
 import re
+from datetime import datetime
+from typing import Any, Dict, Hashable, List, final
+
 import pandas as pd
+
+from src.reading_excel_file import reading_excel
 
 logger = logging.getLogger("services")
 file_handler = logging.FileHandler("../logs/services.log", mode="w", encoding="utf-8")
@@ -15,7 +17,7 @@ logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
 
 
-def profitable_cashback_categories(transactions: List[Dict[Hashable, Any]], year, month):
+def profitable_cashback_categories(transactions: List[Dict[Hashable, Any]], year, month) -> str:
     """Функция считает, какие были более выгодные категории кешбека в выбранном месяце"""
     logger.info("Начало работы функции для поиска более выгодных категорий кешбека")
     if not isinstance(month, str) or not isinstance(transactions, list) or not isinstance(year, str):
@@ -105,11 +107,29 @@ def search_by_phone(transactions: List[Dict[Hashable, Any]]) -> str:
         logger.error("Неверный тип входных данных")
         raise TypeError("Неверный тип входных данных")
 
-    pattern = re.compile(r'\d\s\d{3}\s\d{3}-\d{2}-\d{2}')
+    pattern = re.compile(r"\d\s\d{3}\s\d{3}-\d{2}-\d{2}")
     new_transactions_list = []
-    logger.info("Поиск в словаре транзакций операций по мобильной связи!")
+    logger.info("Поиск в словаре транзакций операций по мобильной связи")
     for transaction in transactions:
         if re.findall(pattern, str(transaction["Описание"])):
+            new_transactions_list.append(transaction)
+    json_data = json.dumps(new_transactions_list, ensure_ascii=False, indent=4)
+    logger.info("Функция отработала успешно!")
+    return json_data
+
+
+def search_by_transfers_to_individuals(transactions: List[Dict[Hashable, Any]]) -> str:
+    """Функция осуществляет поиск переводов физическим лицам"""
+    logger.info("Начало работы функции для поиска переводов физическим лицам")
+    if not isinstance(transactions, list):
+        logger.error("Неверный тип входных данных")
+        raise TypeError("Неверный тип входных данных")
+
+    pattern = re.compile(r"^[А-Яа-я]+\s[А-Я]\.$")
+    new_transactions_list = []
+    logger.info("Поиск в словаре транзакций переводов физическим лицам")
+    for transaction in transactions:
+        if (str(transaction["Категория"]) == "Переводы") and (re.findall(pattern, str(transaction["Описание"]))):
             new_transactions_list.append(transaction)
     json_data = json.dumps(new_transactions_list, ensure_ascii=False, indent=4)
     logger.info("Функция отработала успешно!")
@@ -120,4 +140,5 @@ trans = reading_excel("../data/operations.xlsx")
 # print(investment_bank("2021-12", trans, 50))
 # print(search_by_string(trans, 'Каршеринг'))
 # print(profitable_cashback_categories(trans, "2021", "3"))
-print(search_by_phone(trans))
+# (search_by_phone(trans))
+print(search_by_transfers_to_individuals(trans))
